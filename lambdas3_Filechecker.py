be45c2def0b50ec4d20154a6b2fdb7b3d239916b
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-12-18 15:23:15 +0000

    Practiced re-doing the entire lambda file checker script

--- lambdas3_Filechecker.py
+++ lambdas3_Filechecker.py
@@ -1,23 +1,21 @@
+import boto3
+
 def lambda_handler(event, context):
-    """
-    AWS Lambda function triggered by an S3 event.
-    Logs information about uploaded files and skips non-.txt files.
-    """
+    record = event["Records"][0] #Check first file event.
+    bucket = record["s3"]["bucket"]["name"] #Bucket name
+    file_name = record["s3"]["bucket"]["key"] #Check for file name
 
-    # Get the first record from the S3 event
-    record = event["Records"][0]
-    bucket = record["s3"]["bucket"]["name"]  # Bucket name where the file was uploaded
-    key = record["s3"]["object"]["key"]      # File key (path) inside the bucket
+    if file_name.endswith(".txt"): #Extract by condition .txt
+        s3 = boto3.client("s3") #Connect to aws sdk
+        response = s3.get_object(Bucket=bucket, Key=file_name) #client response
+        text = response["body"].read().decode("utf-8")
+        line_count = len(text.splitlines())
 
-    # Check if the uploaded file is a text file
-    if key.endswith(".txt"):
-        print(f"Received file: {key}")
-        status = f"Processed {key}"
+        print(f"Received file: {file_name}")
+        print(f"Line count: {line_count}")
+
+        return {"file": file_name, "lines": line_count}
+    
     else:
-        print(f"Skipping non-text file: {key}")
-        status = f"Skipped {key}"
-
-    # Return a simple status message
-    return {
-        "status": status
-    }
+        print(f"Skipping non-txt files {file_name}")
+        return {"status": "skipped"}
