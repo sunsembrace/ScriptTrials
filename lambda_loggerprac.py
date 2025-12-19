c0f3efb6c626b67d14c98cdfc1fce65808089c48
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-12-19 21:23:45 +0000

    Added input validation attempt to lambda logger prac

--- lambda_loggerprac.py
+++ lambda_loggerprac.py
@@ -4,8 +4,11 @@ def lambda_handler(event, context):
     a = event.get("a",0)
     b = event.get("b", 0)
 
+    #Validating input practice
+    if not isinstance(a,(int,float)) or not isinstance(b,(int,float)):
+        print(f"Invalid inputs: a={a}, b={b}")
+        return {"error": "Both a and b must be numbers."}
+    
     result = a + b 
-
     print(f"Adding {a} + {b} gives us {result}")
-
     return {"result": result}
