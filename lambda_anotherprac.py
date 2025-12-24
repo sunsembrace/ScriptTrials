a2a48c518d0a426579f1e92b606a92cb894761f3
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-12-24 23:01:11 +0000

    Validation check being added

--- lambda_anotherprac.py
+++ lambda_anotherprac.py
@@ -5,3 +5,6 @@ logger.setLevel(logging.INFO)
 def lambda_handler (event, context):
     message = event.get("message")
     priority = event.get("priority","medium")
+
+    if not isinstance(message,str) or not message:
+        logger.error("Invalid message")
