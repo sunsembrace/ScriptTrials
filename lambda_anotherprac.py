767534f3d96ec2647b7c264327fdd9a0ed4cb9bf
SunsEmbrace <moyasinkhan2000@gmail.com>
2025-12-27 20:21:37 +0000

    Finished another prac.py script

--- lambda_anotherprac.py
+++ lambda_anotherprac.py
@@ -6,5 +6,29 @@ def lambda_handler (event, context):
     message = event.get("message")
     priority = event.get("priority","medium")
 
+    #validate the message
     if not isinstance(message,str) or not message:
-        logger.error("Invalid message")
+        logger.error("Invalid message: must be a non-empty string")
+        return {
+            "error": "Message must be a non-empty string"
+        }
+    
+    #validate priority
+    allowed_priorities = ["low", "medium","High"]
+    if not isinstance(priority, str) or priority not in allowed_priorities:
+        logger.errror(f"Invalid priority: {priority}")
+        return {
+            "Error": "Priority must be one of: low, medium, high"
+        }
+    
+    #format message
+    formatted_message = f"[{priority.upper()}] {message}"
+
+    #log success
+    logger.info(f"Formatted message created: {formatted_message}")
+
+    #return response
+    return {
+        "status": "success:",
+        "formatted message": formatted_message
+        }
