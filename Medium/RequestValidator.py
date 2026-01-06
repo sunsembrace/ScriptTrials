bbae829a593a3a916a547ffcce799067bccddb6b
SunsEmbrace <moyasinkhan2000@gmail.com>
2026-01-06 15:25:08 +0000

    Trying a request validator script but i think i may have done it wrong by trying to implement the AWS service api gateway after rather than before?

---
+++ Medium/RequestValidator.py
@@ -0,0 +1,35 @@
+import boto3
+import logging
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#Import API  gateway, then lambda via lambda handler.
+#import Logging, Import boto3, import JSON since json format
+#Take in input
+# Validate input
+#Return 200, 400.
+#Log throughout whenever we return or come into errors.
+
+api_gw = boto3.client('apigateway')
+
+def lambda_handler(event,context):
+    try:
+        name = event.get('name')
+        age = event.get('age')
+
+        if not isinstance(name,str) or not name:
+            logger.error("Failed": "Invalid name")
+            return {"Status":"Failed", "message": "unable to find valid name"}
+
+        if not isinstance(age,int) or not age:
+            logger.error("Failed":"Invalid name")
+            return {"status": "Failed", "message": "unable to find valid age"}
+            
+    #Validate it into AWS service.
+    #Feel like this is wrong and we weremeant to do it benfore hand.
+
+    except Exception as e:
+        logger.error
+        return 
