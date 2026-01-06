85af7a68082c81bdbbbe4f91011e7a7c689e9d19
SunsEmbrace <moyasinkhan2000@gmail.com>
2026-01-06 19:43:01 +0000

    Changing up planning approach for scripts in ReqValidator

--- Medium/RequestValidator.py
+++ Medium/RequestValidator.py
@@ -1,35 +1,12 @@
-import boto3
-import logging
-import json
+#1.Import Json
+#2Import logging
+#3Take in event via lambda handler.
+#4Validate each step early using status codes such as 200,400.
+#5Parse body.
+#6Validate body.
+#7 Extract specific fields (name, age)
+#8 Validate specific events.
+#9 sucess response via status code
+#10 Ensure wrapped in try, except
+#11 Double check.
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-#Import API  gateway, then lambda via lambda handler.
-#import Logging, Import boto3, import JSON since json format
-#Take in input
-# Validate input
-#Return 200, 400.
-#Log throughout whenever we return or come into errors.
-
-api_gw = boto3.client('apigateway')
-
-def lambda_handler(event,context):
-    try:
-        name = event.get('name')
-        age = event.get('age')
-
-        if not isinstance(name,str) or not name:
-            logger.error("400": "Invalid name")
-            return {"Status":"Failed", "message": "unable to find valid name"}
-
-        if not isinstance(age,int) or not age:
-            logger.error("400":"Invalid name")
-            return {"status": "Failed", "message": "unable to find valid age"}
-            
-    #Validate it into AWS service.
-    #Feel like this is wrong and we weremeant to do it benfore hand.
-
-    except Exception as e:
-        logger.error
-        return 
