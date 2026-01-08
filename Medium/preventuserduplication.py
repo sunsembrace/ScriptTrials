8a3c01f759a0ba059f44fb87e4bc10e35fa323d3
SunsEmbrace <moyasinkhan2000@gmail.com>
2026-01-08 13:57:46 +0000

    Prevent user duplication script planning first

---
+++ Medium/preventuserduplication.py
@@ -0,0 +1,9 @@
+#Import Json
+#Import Logging
+#Import boto3
+#Take in event
+#Extract fields such as name from the event.
+#Compare it against a database
+#IF not currently existing, then creation can occur.
+#Otherwise reject
+#Validate throughout.
