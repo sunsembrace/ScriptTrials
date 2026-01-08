8680174b311634d35fad11f58c97b19ebc525744
SunsEmbrace <moyasinkhan2000@gmail.com>
2026-01-08 14:10:22 +0000

    Importing OS and env variable and a rough skeleton but still WIP

--- Medium/preventuserduplication.py
+++ Medium/preventuserduplication.py
@@ -7,3 +7,35 @@
 #IF not currently existing, then creation can occur.
 #Otherwise reject
 #Validate throughout.
+
+import json
+import os
+import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+DynamoDB = boto3.resources("DynamoDB")
+
+#ENV variable.
+USERS_TABLE = os.environ.get("USERS_TABLE")
+
+#DynamoDB reference table.
+table = DynamoDB.Table( USERS_TABLE)
+
+
+def lambda_handler(event,context):
+    try:
+        #1. Take in event
+        body = event.get("body")
+        json.dumps()
+
+        if not isinstance(body,str) or not body:
+            logger.error("StatusCode", 400)
+            return {"Account creation failed": "User already exists"}
+        
+
+    
+    except Exception as e:
+        logger.error()
